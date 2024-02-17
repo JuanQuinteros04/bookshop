59f2a0beb56c5cde813e52d87281d0ab9cd02779
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-02-16 23:00:42 -0300

    Creating ClientDTO class.

--- src/main/java/com/quinteros/model/dto/ClientDTO.java
+++ src/main/java/com/quinteros/model/dto/ClientDTO.java
@@ -1,4 +1,16 @@
 package com.quinteros.model.dto;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class ClientDTO {
+    private String name;
+
+    private String last_name;
+
+    private Long age;
+
+    private String email;
 }
