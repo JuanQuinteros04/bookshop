5db3c1f6f9e3a17f5a4dc3b1cfeee0dcd78ae957
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-02-17 21:23:08 -0300

    Creating ClientService interface.

--- src/main/java/com/quinteros/service/ClientService.java
+++ src/main/java/com/quinteros/service/ClientService.java
@@ -1,7 +1,20 @@
 package com.quinteros.service;
 
+import com.quinteros.model.dto.ClientDTO;
+import com.quinteros.model.dto.ClientResponse;
+
 import java.util.List;
 
 public interface ClientService {
+
+    List<ClientResponse> findAll();
+
+    ClientResponse findById(Long id);
+
+    ClientResponse createClient(ClientDTO clientDTO);
+    void updateClient(Long id, ClientDTO clientDTO);
+
+    void deleteClient(Long id);
+
 
 }
