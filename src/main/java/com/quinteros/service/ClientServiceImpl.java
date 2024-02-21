913d9b4cf405ce7bd06dd9b4f8de3995de891a49
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-02-20 23:42:24 -0300

    The ClientServiceImpl class has been finalized.

--- src/main/java/com/quinteros/service/ClientServiceImpl.java
+++ src/main/java/com/quinteros/service/ClientServiceImpl.java
@@ -49,6 +49,6 @@ public class ClientServiceImpl implements ClientService{
 
     @Override
     public void deleteClient(Long id) {
-
+        clientRepository.delete(clientRepository.findById(id).orElseThrow(RuntimeException::new));
     }
 }
