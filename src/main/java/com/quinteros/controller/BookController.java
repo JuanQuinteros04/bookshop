aa19b69286012d90f909341ee2ce93eec961c59c
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-02-22 18:43:03 -0300

    Get methods added to BookController controller.

--- src/main/java/com/quinteros/controller/BookController.java
+++ src/main/java/com/quinteros/controller/BookController.java
@@ -1,12 +1,32 @@
 package com.quinteros.controller;
 
+import com.quinteros.model.dto.BookResponse;
 import com.quinteros.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
+@RequestMapping("/books")
 public class BookController {
 
     @Autowired
     BookService bookService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<List<BookResponse>> findAll(){
+      return ResponseEntity.ok(bookService.findAll());
+    };
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<BookResponse> findBookById(@PathVariable("id")Long id){
+      return ResponseEntity.ok(bookService.findBookById(id));
+    };
+
 }
