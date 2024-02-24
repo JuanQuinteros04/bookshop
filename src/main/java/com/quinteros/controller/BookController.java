3fd2c24f12bf76655551be3acabe33b85b8ade68
JuanQuinteros04 <juanmanuelquinteros3@gmail.com>
2024-02-24 15:10:17 -0300

    Box Controller class created, the mapping of the HTTP methods with their respective BookService function was carried out.

--- src/main/java/com/quinteros/controller/BookController.java
+++ src/main/java/com/quinteros/controller/BookController.java
@@ -1,14 +1,12 @@
 package com.quinteros.controller;
 
+import com.quinteros.model.dto.BookDTO;
 import com.quinteros.model.dto.BookResponse;
 import com.quinteros.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -28,5 +26,22 @@ public class BookController {
     public ResponseEntity<BookResponse> findBookById(@PathVariable("id")Long id){
       return ResponseEntity.ok(bookService.findBookById(id));
     };
+
+    @PutMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<BookResponse> createBook(BookDTO bookDTO){
+        return ResponseEntity.ok(bookService.createBook(bookDTO));
+    }
+
+    @PostMapping(value = "/{id}",produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateBook(@PathVariable("id") Long id,BookDTO bookDTO){
+        bookService.updateBook(id, bookDTO);
+        return ResponseEntity.status(204).build();
+    }
+
+    @DeleteMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> deleteBook(@PathVariable("id") Long id){
+        bookService.deleteBook(id);
+        return ResponseEntity.status(204).build();
+    }
 
 }
